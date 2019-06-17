dc2ad50579fb1307791f279f3abb461a9f06b6a6
Neuf-Life <reboul.blaise@gmail.com>
2019-06-17 15:45:43 +0400

    Modif dans le suggestionController, modif du formulaire suggestions, création du chemin formulaire dans web.php

--- app/Http/Controllers/SuggestionsController.php
+++ app/Http/Controllers/SuggestionsController.php
@@ -1,11 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Suggestion;
 
 use Illuminate\Http\Request;
+use App\Suggestion;
 
 class SuggestionsController extends Controller
-{
-    //
+{  
+    // ----------------------------- CRUD SUGGESTIONS -----------------------------
+    // FORMULAIRE SUGGESTION -------------------------
+    public function form_suggestions(){
+        $suggestions = Suggestion::get();
+        return view("suggestions",["suggestions"=> $suggestions]);
+    }
+    // CREATE SUGGESTION -----------------------------
+    public function store_suggestion(Request $request){
+        dd($request->all());
+        $suggestion = new Suggestion;
+        $request->validate([
+            "titre"=> "required",
+            "message"=>"required|max:500",
+        ]);
+        $suggestion->titre = $request->titre;
+        $suggestion->message = $request->message;
+        $suggestion->save();
+        //return redirect()->action("ProductController@indexall");
+    }
+    // FIN CREATE SUGGESTION --------------------------
+    // ----------------------------- FIN CRUD SUGGESTIONS -----------------------------
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/suggestions', 'SuggestionsController@form_suggestions');
+
+Route::post('/suggestions/create', 'SuggestionsController@store_suggestion');
