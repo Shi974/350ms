b63fc5c7cf30e8b5d53765e03feb5ca9c25a2573
Shi974 <shidl@rocketmail.com>
2019-06-19 15:53:19 +0400

    js effect + event id css responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     
     // FONCTION JQUERY HOVER IMAGE
-    // class="events_image"
     $('.events_image').hover(function() {
         $(".events_image").addClass('img_hover');
     }, function() {
