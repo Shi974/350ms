d44e192da7ebf8bfd795e70760106676523aa54b
Shi974 <shidl@rocketmail.com>
2019-06-20 15:56:44 +0400

    no_access

--- resources/views/no_access.blade.php
+++ resources/views/no_access.blade.php
@@ -3,8 +3,8 @@
 
 <div class="jumbotron bg-danger text-white rounded-0" id="no_access">
     <div class="container">
-        <h3 class="display-3 text-white">Accès refusé</h3>
-        <p>Droits insuffisants pour accéder à la page demandée.<p>
+        <h3 class="display-3 text-white"><i class="fas fa-skull"></i> Game Over ! <i class="fas fa-skull"></i></h3>
+        <p>Accès refusé ! Vos droits sont insuffisants pour accéder à la page demandée.<p>
     </div>
 </div>
 
