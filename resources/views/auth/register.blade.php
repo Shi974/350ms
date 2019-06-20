e0a3224fcbb6f9d535913cc2b4c3c1e9bc36e7bb
Hivy <hivy55@gmail.com>
2019-06-20 23:08:57 +0400

    required class register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('Prenom') }}</label>
+                            <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('Prenom') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror"
@@ -27,7 +27,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="lastname" type="text"
@@ -44,7 +44,7 @@
 
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
@@ -60,7 +60,7 @@
 
                         <div class="form-group row">
                             <label for="phone"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Téléphone') }}</label>
+                                class="col-md-4 col-form-label text-md-right">{{ __('Téléphone') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="phone" type="number" class="form-control @error('phone') is-invalid @enderror"
@@ -78,7 +78,7 @@
 
 
                         <div class="form-group row">
-                            <label for="pseudo" class="col-md-4 col-form-label text-md-right">{{ __('Pseudo') }}</label>
+                            <label for="pseudo" class="col-md-4 col-form-label text-md-right">{{ __('Pseudo') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="pseudo" type="text" class="form-control @error('pseudo') is-invalid @enderror"
@@ -94,7 +94,7 @@
 
                         <div class="form-group row">
                             <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
+                                class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password"
@@ -111,7 +111,7 @@
 
                         <div class="form-group row">
                             <label for="password-confirm"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Confirmation mot de passe') }}</label>
+                                class="col-md-4 col-form-label text-md-right">{{ __('Confirmation mot de passe') }}<span class="required">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control"
